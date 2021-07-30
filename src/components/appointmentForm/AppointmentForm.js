c5bbe6f197214cffe8b1b97c46b84bc63239ba18
Melissa Mondot <31637708+melissa-mondot@users.noreply.github.com>
2021-07-29 18:41:48 -0700

    Form ready for props

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -2,6 +2,7 @@ import React from "react";
 
 export const AppointmentForm = ({
   contacts,
+  contactNames,
   title,
   setTitle,
   contact,
@@ -10,7 +11,7 @@ export const AppointmentForm = ({
   setDate,
   time,
   setTime,
-  handleSubmit
+  handleSubmit,
 }) => {
   const getTodayString = () => {
     const [month, day, year] = new Date()
@@ -18,8 +19,55 @@ export const AppointmentForm = ({
       .split("/");
     return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
   };
+  const getTimeString = new Date().toLocaleTimeString("en-US", {
+    hour12: false,
+  });
+  const optionList = contactNames.map((name) => <option key={name}>{name}</option>);
 
   return (
-    AppointmentForm
+    <>
+      <form name="appointmentForm" onSubmit={handleSubmit}>
+        <label>
+          Title:
+          <input
+            required
+            autoFocus
+            placeholder="Meeting Goal"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </label>
+        <label>
+          Contact:
+          <select
+            required
+            value={contact}
+            onChange={(e) => setContact(e.target.value)}
+          >
+            {/* generated contact name list here */}
+            {optionList}
+          </select>
+        </label>
+        <label>
+          Date:
+          <input
+            required
+            type="date"
+            value={getTodayString() || date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </label>
+        <label>
+          Time:
+          <input
+            required
+            type="time"
+            value={getTimeString || time}
+            onChange={(e) => setTime(e.target.value)}
+          />
+        </label>
+        <button>Submit Contact</button>
+      </form>
+    </>
   );
 };
